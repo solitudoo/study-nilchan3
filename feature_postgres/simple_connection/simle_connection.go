068cccd9c6465c57b1c725c49d39ca5d1256b15a
Zaurbek Bisliev <zbisliyev@mail.ru>
2026-09-15 22:33:16 +0300

    simple sql

--- feature_postgres/simple_connection/simle_connection.go
+++ feature_postgres/simple_connection/simle_connection.go
@@ -2,22 +2,10 @@ package simple_connection
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/jackc/pgx/v5"
 )
 
-func CheckConnection() {
-	ctx := context.Background()
-
-	conn, err := pgx.Connect(ctx, "postgres://postgres:pfeh,tr,bc9595@localhost:5432/postgres")
-
-	if err != nil {
-		panic(err)
-	}
-
-	if err := conn.Ping(ctx); err != nil {
-		panic(err)
-	}
-	fmt.Println("Подключение в базе данных прошло успешно")
+func CreateConnection(ctx context.Context) (*pgx.Conn, error) {
+	return pgx.Connect(ctx, "postgres://postgres:pfeh,tr,bc9595@localhost:5432/postgres")
 }
---
+++ feature_postgres/simple_sql/simple_delete.go
@@ -0,0 +1,15 @@
+package simple_sql
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func DeleteRow(ctx context.Context, conn *pgx.Conn, tasksIDs []int) error {
+	sqlQuery := `
+    DELETE FROM tasks WHERE id = ANY($1);`
+
+	_, err := conn.Exec(ctx, sqlQuery, tasksIDs)
+	return err
+}
